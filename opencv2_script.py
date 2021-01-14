185350be5385c97598db4d8d088a1675b940b015
gragundier <gragundier@gmail.com>
2021-01-14 16:36:33 -0500

    lol  forgot to adjust capture

--- opencv2_script.py
+++ opencv2_script.py
@@ -2,18 +2,19 @@ import cv2
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--video", type=str, default="01293907", help="video to extract from")
+parser.add_argument("--dir", type=str, default="/home/gragundier/Videos/ffxiv_fisher", help="directory to find video in")
+parser.add_argument("--video", type=str, default="sample1", help="video to extract from")
 
 args = parser.parse_args()
 
 # Opens the Video file
-cap= cv2.VideoCapture('{}.mkv'.format(args.video))
+cap= cv2.VideoCapture('{}/{}.mkv'.format(args.dir, args.video))
 i=0
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == False:
         break
-    cv2.imwrite('/home/gragundier/Videos/ffxiv_timelapses/{}/{}_'.format(args.video, args.video)+str(i)+'.jpg',frame)
+    cv2.imwrite('{}/{}/{}_'.format(args.dir, args.video, args.video)+str(i)+'.png',frame)
     i+=1
  
 cap.release()
