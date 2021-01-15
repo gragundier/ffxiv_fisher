cc0891fd81c0f9ac8bbf847d4db2895d73904ab5
Peter Ro <ropeter@cse.msu.edu>
2021-01-15 18:21:18 -0500

    changed the inferscript to meet new model needs

--- inferscript.py
+++ inferscript.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import time
 import numpy as np
 import keyboard
 #import mss
@@ -9,28 +10,26 @@ from mss.windows import MSS as mss
 monitor = {"top": 316, "left": 848, "width": 224, "height": 224}
 output = "sct-{top}x{left}_{width}x{height}.png".format(**monitor)
 
-model = tf.keras.models.load_model("model")
+model = tf.keras.models.load_model("C:/Users/gragundier/Models/model1")
 
 with mss() as sct:
     try:
+        sct_old = None
         while True:
             sct_img = np.array(sct.grab(monitor).pixels).reshape(1, 224,224,3)
-            #print(sct_img.shape)
-            #print(model.predict(sct_img))
-            #mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
-            classification = model.predict(sct_img)[0]
-            if classification[0] == 1:
-                keyboard.send("2")
-            elif classification[1] == 1:
-                keyboard.send("q")
-            elif classification[2] == 1:
-                keyboard.send("e")
-            elif classification[3] == 1:
-                keyboard.send("e")
-            elif classification[3] == 1:
-                keyboard.send("e")
-            elif classification[4] == 1:
-                keyboard.send("e")
+            if sct_old is not None:
+                img = sct_old-sct_img
+                #print(sct_img.shape)
+                #print(model.predict(sct_img))
+                #mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
+                classification = model.predict(img)[0]
+                #if classification[0] == 1:
+                    #keyboard.send("2")
+                if classification[1] == 1:
+                    keyboard.send("q")
+                    time.sleep(5)
+                    keyboard.send("f")
+            sct_old = sct_img
 
     except KeyboardInterrupt:
         print("KeyboardInterrupt")
